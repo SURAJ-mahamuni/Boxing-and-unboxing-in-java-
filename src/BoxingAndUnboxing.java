67b6d44ccc4eb5bfd4af2a375a40913d6c25de54
Suraj <surajmaha00@gmail.com>
2022-09-21 20:58:04 +0530

    boxing and unboxing

--- src/BoxingAndUnboxing.java
+++ src/BoxingAndUnboxing.java
@@ -8,7 +8,16 @@ public class BoxingAndUnboxing {
         Long l = new Long(400);
         Short sh = new Short((short)7187);
         Boolean bl = new Boolean((boolean) TRUE);
+
         System.out.println(bl);
+        Float fl = new Float(30.00f);
+        Byte bt = new Byte((byte) 1);
+
+        System.out.println(bt);
+        Double db = new Double(34);
+        Character ch = new Character('c');
+
+
 
         //2
         Integer i1 = 10;
